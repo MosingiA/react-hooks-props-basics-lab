9d3916e54a79c93c1b50926b7d095af8d296917e
MosingiA <ondicho33@gmail.com>
2025-07-13 08:02:10 +0300

    react-hooks-props-basics-lab

---
+++ src/components/Links.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import User from '../data/user';
+import About from './About';
+
+
+function Links(props) {
+  return (
+    <div id="links">
+      <h3>Links</h3>
+        <a href={props.github}>{props.github}</a><br/>
+        <a href={props.linkedin}>{props.linkedin}</a>
+        </div>
+        );
+  }
+
+  export default Links;
